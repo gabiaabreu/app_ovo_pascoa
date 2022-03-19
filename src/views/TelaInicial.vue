005ffb8184dd9083c6175f1b9cc892b3f6b2f89f
MarcosTsujino <marcos-tsu_jino@hotmail.com>
2022-03-19 12:08:43 -0300

    Incrementação imagem

---
+++ src/views/TelaInicial.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app-top-bar">
+    <v-app-bar app color="brown darken-4">
+      <v-toolbar-title>Nome do App</v-toolbar-title>
+      <h1>Seja Bem-vinde!</h1>
+      <v-img src="../assets/HappyEaster.jpg" alt="Happ Easter" > 
+        
+      </v-img>
+    </v-app-bar>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
