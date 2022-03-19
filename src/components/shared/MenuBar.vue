d870bbc49486ff28b183c51c81280f763357e59d
Gabriela Abreu <gabiabr3u@gmail.com>
2022-03-19 12:54:30 -0300

    Finalizando barra lateral e menu

--- src/components/shared/MenuBar.vue
+++ src/components/shared/MenuBar.vue
@@ -26,19 +26,34 @@
           v-model="group"
           active-class="brown--text text--lighten-2"
         >
+
+        <router-link to="/">
           <v-list-item>
             <v-list-item-icon>
               <v-icon>mdi-home</v-icon>
             </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
+            <v-list-item-title>Início</v-list-item-title>
           </v-list-item>
+        </router-link>
 
+        <router-link to="/cadastro">
           <v-list-item>
             <v-list-item-icon>
               <v-icon>mdi-account</v-icon>
             </v-list-item-icon>
-            <v-list-item-title>Account</v-list-item-title>
+            <v-list-item-title>Conta</v-list-item-title>
           </v-list-item>
+        </router-link>
+
+        <router-link to="/listaovos">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-egg-easter</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Ovos de Páscoa</v-list-item-title>
+          </v-list-item>
+        </router-link>
+
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
@@ -53,3 +68,9 @@ export default {
     })
 }
 </script>
+
+<style scoped>
+.menu-bar a {
+    text-decoration: none;
+}
+</style>
