718bf09bc20339c275485c664f7cf2a4d7998b7d
Priscila Padilla Latance <priscila.latance@gmail.com>
2022-03-19 13:29:15 -0300

    Atualização Lista Ovos

--- src/views/ListaOvos.vue
+++ src/views/ListaOvos.vue
@@ -3,13 +3,13 @@
         <h2 class = "text-h5 text-center mb-3 mt-5">Classificação</h2>
       <ul>
         <li v-for="lista of listaOvos" :key="lista.nome">
-         <v-img :src="lista.imagem" max-height="150" max-width="250"></v-img>
-         <p>{{lista.nome}}</p>
-         <p>Preço: R${{lista.preco}}</p>
-         <p>Sabor: {{lista.sabor}}</p>
-         <p>Cep: {{lista.local.cep}}</p>
-         <p>Endereço: {{lista.local.endereco}}</p>
-
+            <v-img :src="lista.imagem" max-height="150" max-width="250"></v-img>
+            <p>{{lista.nome}}</p>
+            <p>Preço: R${{lista.preco}}</p>
+            <p>Sabor: {{lista.sabor}}</p>
+            <p>Cep: {{lista.local.cep}}</p>
+            <p>Endereço: {{lista.local.endereco}}</p>
+            <v-btn outlined rounded color="brown--text text--lighten-2" elevation="2">+ Adicionar ao Carrinho</v-btn>
         </li>
       </ul>
 
