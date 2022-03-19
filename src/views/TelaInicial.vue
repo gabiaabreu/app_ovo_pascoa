5676d37239bd141dcf87b8ae6debcb9815a47010
MarcosTsujino <marcos-tsu_jino@hotmail.com>
2022-03-19 12:45:04 -0300

    Atualização Tela Inicial

--- src/views/TelaInicial.vue
+++ src/views/TelaInicial.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="app-top-bar">
     <v-app-bar app color="brown darken-4">
+      <v-app-icon></v-app-icon>
       <v-toolbar-title>Nome do App</v-toolbar-title>
       <h1>Seja Bem-vinde!</h1>
       <v-img src="../assets/HappyEaster.jpg" alt="Happ Easter" > 
