41b305d6dbd1d8fb267f3beff4ca3c58a0b6e656
Gabriela Abreu <gabiabr3u@gmail.com>
2022-03-19 12:23:33 -0300

    Implementando Menu Bar

--- src/components/shared/MenuBar.vue
+++ src/components/shared/MenuBar.vue
@@ -1,4 +1,55 @@
 <template>
-    <div>
-    </div>
+    <div class="menu-bar">
+        <v-app-bar app
+      color="brown darken-4"
+      dense
+      dark
+    >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Ovos.com</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+
+      <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="brown--text text--lighten-2"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
 </template>
+
+<script>
+export default {
+    data: () => ({
+      drawer: false,
+      group: null,
+    })
+}
+</script>
