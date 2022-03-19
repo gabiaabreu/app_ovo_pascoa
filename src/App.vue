7c0d8645abfdd18343f8bbfe39edfd5fac4a524f
Priscila Padilla Latance <priscila.latance@gmail.com>
2022-03-19 11:32:43 -0300

    Teste

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <v-app>
     <MenuBar>
       <v-main></v-main>
@@ -18,23 +17,4 @@ export default {
 
 <style lang="scss">
 
-=======
-<v-app>
-
-
-</v-app>
-
-  
-</template>
-
-<script>
-export default {
-  name: 'App',
-  
-}
-</script>
-
-<style>
-
->>>>>>> c2a35026794fc9af8af0c229e2fdf646970de7e1
 </style>
