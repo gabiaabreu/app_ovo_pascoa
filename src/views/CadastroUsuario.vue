79baef2d97d9590f38d97f00844672a067e8f663
Katherine Sousa Pedreira <katherinesousapedreira@MacBook-Pro-de-Katherine.local>
2022-03-19 13:00:01 -0300

    Adicionando botoes de cadastro e login

--- src/views/CadastroUsuario.vue
+++ src/views/CadastroUsuario.vue
@@ -1,21 +1,36 @@
 <template >
-<h1> cadastro </h1>
+<div>
+    <h1> cadastro </h1>
+
+    <v-tabs fixed-tabs background-color="blue-grey lighten-5" color="black" v-model="tabsForm">
+
+        <v-tab key="register"> Cadastrar </v-tab>
+        <v-tab key="login"> Login </v-tab>
+
+    </v-tabs>
+
+</div>
+
+
 </template>
+
+
     
 
 
 <script>
 export default {
-    name: 'CadastroUsuario'
+    name: 'CadastroUsuario',
+    data: () => ({
+        tabsform: null
+    })
    
 }
 </script>
 
 <style scoped>
 
-body {
-    background-color: blue;
-}
+
 
 
 </style>
